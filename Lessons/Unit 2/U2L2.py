38ff76891d589914af00c777709d611fed2be840
Carter <756118@pdsb.net>
2025-09-19 13:30:01 -0400

    Update U2L2.py

--- Lessons/Unit 2/U2L2.py
+++ Lessons/Unit 2/U2L2.py
@@ -5,3 +5,12 @@ inches = 10 ** 12
 (earth_to_moon, _miles) = divmod(miles, 238855)
 
 print("One trillion inches is the same as going the moon and back %d times, plus an extra %d miles, %d yards, %d feet, and %d inches." % (earth_to_moon, _miles, _yards, _feet, _inches ))
+
+def toLower(letter):
+    if ord(letter) >= 65 and ord(letter) <= 90:
+        return chr(ord(letter) + 32)
+    print("The letter is not uppercase.")
+    return " "
+
+print(toLower('A'))
+print(toLower('a'))
