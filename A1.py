bc80c61f08ca7dc4657fae249043c1a7be9dbb8c
Carter <756118@pdsb.net>
2025-10-21 23:54:26 -0400

    Update A1.py

--- A1.py
+++ A1.py
@@ -15,15 +15,72 @@ def addM(M1, M2):
         result.append(row)
     return result
 
+def is_prime(num):
+    return (num == 5) or (num % 2 != 0 and num % 3 != 0 and num % 5 != 0)
+
+def print_matrix(M):
+    largest = 0
+    for i in range(len(M)):
+        for j in range(len(M)):
+            if M[i][j] > largest:
+                largest = M[i][j]
+    
+    width = len(str(largest))
+    
+    for i in range(len(M)):
+        for j in range(len(M)):
+            print("%*d " % (width, M[i][j]), end="")
+        print()
+
+def magic_sum_formula(N):
+    total = 0
+    for i in range(1, N * N + 1):
+        total = total + i
+    return total // N
+
+def is_magic_square(M):
+    size = len(M)
+    
+    magic_sum = magic_sum_formula(size)
+
+    for i in range(size):
+        row_sum = 0
+        for j in range(size):
+            row_sum += M[i][j]
+        if row_sum != magic_sum:
+            return False
+
+    for j in range(size):
+        col_sum = 0
+        for i in range(size):
+            col_sum += M[i][j]
+        if col_sum != magic_sum:
+            return False
+
+    diag1 = 0
+    diag2 = 0
+    for i in range(size):
+        diag1 += M[i][i]
+        diag2 += M[i][size - 1 - i]
+
+    if diag1 != magic_sum or diag2 != magic_sum:
+        return False
+
+    return True
+
 valid_input = False
 matrix_size = 0
 while not valid_input:
     user_input = input("Enter a prime number between 5 and 19: ")
-    if user_input.isdigit() and int(user_input) >= 5 and int(user_input) <= 19:
+    if user_input.isdigit():
         user_input_int = int(user_input)
-        if user_input_int % 2 != 0 and user_input_int % 3 != 0 and user_input_int % 5 != 0:
+        if user_input_int >= 5 and user_input_int <= 19 and is_prime(user_input_int):
             valid_input = True
             matrix_size = user_input_int
+        else:
+            print("Input must be an odd prime number between 5 and 19.")
+    else:
+        print("Please enter a number.")
 
 M1 = []
 row = []
@@ -40,9 +97,16 @@ for i in range(0, matrix_size):
 random.shuffle(row)
 for i in range(0, matrix_size * 3 + 1, 3):
     M2.append(shift(row,i))
-    
+    5
 M3 = addM(M1, M2)
-for row in M3:
-    for num in row:
-        print(num, end=" ")
-    print()
+
+print("\nMagic Square of size %d\n------------------------" % matrix_size)
+print_matrix(M3)
+
+magic_sum = magic_sum_formula(matrix_size)
+print("\nMagic Sum: %d" % magic_sum)
+
+if is_magic_square(M3):
+    print("The square is magic!")
+else:
+    print("The square is not magic!")
