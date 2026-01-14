2ee18718a1c53ce53a5a624a55aad2a097033e45
Carter <756118@pdsb.net>
2026-01-14 13:00:01 -0500

    Update A3.py

--- Assignment 3/A3.py
+++ Assignment 3/A3.py
@@ -2,9 +2,9 @@ class Term:
     def __init__(self, coeff, expo):
         self.__coeff = coeff
         self.__expo = expo
-    
+        
     def is_valid(self):
-        if self.__expo >= 0:
+        if isinstance(self.__coeff, (int, float)) and isinstance(self.__expo, int) and self.__expo >= 0:
             return True
         return False
     
@@ -27,12 +27,25 @@ class Polynomial:
         self.__terms = self.str_to_list(poly_str)
         self.__order = len(self.__terms) - 1
     
+    def is_valid(self):
+        for term in self.__terms:
+            if not term.is_valid():
+                return False
+        return True
+        
     def str_to_list(self, poly_str):
         poly_list = poly_str.split(" ")
         terms_list = []
         for i in range(len(poly_list)):
-            term_int = int(poly_list[i])
-            new_term = Term(term_int, len(poly_list) - i - 1)
+            term_num = 0
+            try:
+                try:
+                    term_num = int(poly_list[i])
+                except ValueError:
+                    term_num = float(poly_list[i])
+            except ValueError:
+                term_num = None
+            new_term = Term(term_num, len(poly_list) - i - 1)
             terms_list.append(new_term)
         return terms_list
             
@@ -57,8 +70,30 @@ class Polynomial:
                 if i < len(self.__terms) - 1:
                     result += " + "
         return result
-            
-            
-poly = Polynomial("-1 2 8 0 2")
-print(poly)
-print(poly.calculate_area(-2, 4))
+
+def get_numeric_interval(interval_str):
+    try:
+        x1, x2 = interval_str.split(" ")
+        return (float(x1), float(x2))
+    except:
+        return (None, None)
+
+poly = None
+user_input_poly = None
+input_x1 = None
+input_x2 = None
+
+valid_input = False
+while not valid_input:
+    user_input_poly = input("Enter a Polynomial (coefficients separated by a space): ").strip()
+    user_input_interval = input("Enter a Interval (x1 and x2 separated by a space): ").strip()
+    poly = Polynomial(user_input_poly)
+    input_x1, input_x2 = get_numeric_interval(user_input_interval)
+    if poly.is_valid() and input_x1 != None and input_x2 != None:
+        valid_input = True
+    else:
+        print("Bad input please try again.")
+    
+print("--------------------------\n" + str(poly) + "\n--------------------------")
+print("Area under the interval {0}, {1}: \n{2}".format(input_x1, input_x2, poly.calculate_area(input_x1, input_x2)))
+    
